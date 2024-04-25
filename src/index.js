87217c28aefe0d96c2e33c0cae37b15d5b7c33a2
MbekiHildah <mbekihildah@gmail.com>
2024-04-25 20:01:19 +0100

    Tidied up css and javascript

--- src/index.js
+++ src/index.js
@@ -16,6 +16,8 @@ function refreshWeather(response) {
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windElement.innerHTML = `${response.data.wind.speed}km/h`;
   iconElement.innerHTML = `<img src="${response.data.condition.icon_url}"class="weather-app-icon"/>`;
+
+  getForecast(response.data.city);
 }
 function formatDate(date) {
   let minutes = date.getMinutes();
@@ -58,25 +60,38 @@ document.querySelector("form").addEventListener("submit", handleSearchSubmit);
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 function getForecast(city) {
   let apiKey = "2ft2755f62423283e36ab5068a024o8d";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}`;
   axios(apiUrl).then(displayForecast);
 }
-function displayForecast() {
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+function displayForecast(response) {
   let forecastHtml = "";
 
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `<div class="weather-forecast-day">
-<div class="weather-forecast-date">${day}</div>
-<div class="weather-forecast-temperature">28°</div>
-<div class="weather-forecast-icon">❄️</div></div>`;
+  response.data.daily.forEach(function (day, index) {
+    if (index < 3) {
+      forecastHtml =
+        forecastHtml +
+        `
+      <div> <div class="weather-forecast-date">${formatDay(day.time)}</div>
+<div class="weather-forecast-temperature">${Math.round(
+          day.temperature.maximum
+        )}°</div>
+<div><img src="${
+          day.condition.icon_url
+        }" class="weather-forecast-icon" /> </div></div>
+`;
+    }
   });
-  let forecastElement = document.querySelector("#forecast-container");
+
+  let forecastElement = document.querySelector("#forecast");
   forecastElement.innerHTML = forecastHtml;
 }
-
-displayForecast();
+searchCity("Celbridge");
